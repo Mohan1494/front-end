a2be241c5433e7711819c8904cb0333d5af68732
Mohan1494 <115537385+Mohan1494@users.noreply.github.com>
2025-04-05 12:02:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     setResult(null);
 
     try {
-      const response = await fetch('https://bert-model-api.onrender.com/predict', { // Update URL to match backend
+      const response = await fetch('https://model-api-rwbd.onrender.com/predict', { // Update URL to match backend
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
